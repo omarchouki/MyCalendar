8a326efb961ef8f3e47c70542cb3832a1918a7cf
omarchouki1993@gmail.com <omarchouki1993@gmail.com>
2023-04-17 03:03:11 +0100

    Improve backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, json
-from flask_mysqldb import MySQL
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from config import Config
@@ -13,6 +14,12 @@ from random import randint
 def create_app(config_class=Config):
 
     app = Flask(__name__)
+    auth = HTTPBasicAuth()
+
+    users = {
+        "user": generate_password_hash("user")
+    }
+
     app.config.from_object(config_class)
     CORS(app)
     db.init_app(app)
@@ -21,6 +28,12 @@ def create_app(config_class=Config):
         range_start = 10**(n-1)
         range_end = (10**n)-1
         return randint(range_start, range_end)
+
+    @auth.verify_password
+    def verify_password(username, password):
+        if username in users and \
+                check_password_hash(users.get(username), password):
+            return username
 
     @app.route('/reservations', methods=['POST'])
     def create_reservation():
@@ -62,51 +75,21 @@ def create_app(config_class=Config):
 
     @app.route('/reservations/<int:id>', methods=['PUT'])
     def update_reservation(id):
-        cur = mysql.connection.cursor()
         date = request.json.get("date")
         heure_debut = request.json.get("heure_debut")
         heure_fin = request.json.get("heure_fin")
         description = request.json.get("description")
         email = request.json.get("email")
-        query = "UPDATE disponibilites SET "
-        if date:
-            query += "date = %s, "
-        if heure_debut:
-            query += "heure_debut = %s, "
-        if heure_fin:
-            query += "heure_fin = %s, "
-        if description:
-            query += "description = %s, "
-        if email:
-            query += "email = %s, "
-        query = query[:-2] + " WHERE id = %s"
-        values = []
-        if date:
-            values.append(date)
-        if heure_debut:
-            values.append(heure_debut)
-        if heure_fin:
-            values.append(heure_fin)
-        if description:
-            values.append(description)
-        if email:
-            values.append(email)
-        values.append(id)
-        cur.execute(query, tuple(values))
-        mysql.connection.commit()
-        cur.close()
+        
         return jsonify({'message': 'Disponibilité mise à jour avec succès'})
 
-    # Endpoint pour supprimer une disponibilité existante
+    # Endpoint pour supprimer une reservation existante
     @app.route('/reservations/<int:id>', methods=['DELETE'])
+    @auth.login_required
     def delete_reservation(id):
-        cur = mysql.connection.cursor()
-
-        # Exécuter la requête de suppression
-        query = "DELETE FROM disponibilites WHERE id=%s"
-        cur.execute(query, (id,))
-        mysql.connection.commit()
-
+        reservation = Reservation.query.get(id)
+        db.session.delete(reservation)
+        db.session.commit()
         # Retourner un message de confirmation
         return jsonify({'message': 'Availability deleted'})
 
